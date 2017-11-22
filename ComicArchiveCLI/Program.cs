4ac8b320135e411fcfd4c2f0affea0b586235036
Craig Reynolds <craig@wyldphyre.com>
2017-11-22 20:14:07 +0800

    More testing code

--- ComicArchiveCLI/Program.cs
+++ ComicArchiveCLI/Program.cs
@@ -8,12 +8,30 @@ namespace ComicArchiveCLI
     {
       var converter = new ComicArchive.Converter();
       var options = new ComicArchive.ConverterOptions();
+      options.OverwriteExisting = true;
+      //options.LogOptions.ShowFullPaths = true;
 
       // TODO: Implement command arguments of: Convert, Read, and (eventually) Write
+      
+      converter.LogActivityEvent += (message) => Console.WriteLine(message);
 
-      //converter.ConvertToZipArchive(@"F:\Media\Comics\Testing\Big Man Plans\Big Man Plans 01 (of 04) (2015) (digital) (Son of Ultron-Empire).cbr", options, out var convertedPath);
+      try
+      {
+        //converter.ConvertToZipArchive(@"F:\Media\Comics\Testing\Big Man Plans\Big Man Plans 01 (of 04) (2015) (digital) (Son of Ultron-Empire).cbr", options, out var convertedPath);
+        //var (result, message) = converter.ConvertToZipArchive(@"/Users/craigreynolds/Desktop/comictest/Big Hero 6 01 (of 05) (2008) (Digital) (AnPymGold-Empire).cbz", options, out var convertedPath);
+        //var (result, message) = converter.ConvertToZipArchive(@"/Users/craigreynolds/Desktop/comictest/Big Hero 6 01 (of 05) (2008) (Digital) (AnPymGold-Empire).zip", options, out var convertedPath);
 
-      Console.WriteLine("Hello World!");
+        // if (!string.IsNullOrWhiteSpace(message))
+        //   Console.WriteLine(message);
+      }
+      catch (Exception e)
+      {        
+        Console.WriteLine($"Exception: {e.Message}\n\n");
+        Console.WriteLine(e.StackTrace);
+      }
+
+      Console.WriteLine(string.Empty);
+      Console.WriteLine("ComicArchive finshed!");
     }
   }
 }
