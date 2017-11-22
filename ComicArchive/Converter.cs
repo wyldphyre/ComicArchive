ea033f6d06cad3b5233054bd7c1b9d6f018fd684
Craig Reynolds <craigr@kestral.com.au>
2017-11-22 15:51:45 +0800

    Start of handling replacing existing files

--- ComicArchive/Converter.cs
+++ ComicArchive/Converter.cs
@@ -12,6 +12,7 @@ namespace ComicArchive
   {
     public bool ReplaceOriginalFile { get; set; }
     public string WorkingPath { get; set; }
+    public bool OverwriteExisting { get; set; }
   }
 
   public class Converter
@@ -25,42 +26,63 @@ namespace ComicArchive
       var sourceFilename = Path.GetFileNameWithoutExtension(sourcePath);
       convertedPath = Path.Combine(sourceDirectory, sourceFilename + ".cbz");
 
-      if (File.Exists(convertedPath))
+      if (File.Exists(convertedPath) && !options.OverwriteExisting)
         throw new Exception($"Cannot convert path '{sourcePath} to Zip. Target path '{convertedPath}' already exists");
 
-      //TODO: check if file is already a zip archive and has the proper extension do nothing
+      var FileIsZip = ZipArchive.IsZipFile(sourcePath);
 
-      var workingPath = Path.Combine(options.WorkingPath ?? AppContext.BaseDirectory, sourceFilename);
+      //TODO: Confirm that if an archive is already a zip file and has the proper extension then it is skipped
+      if (FileIsZip && string.Equals(Path.GetExtension(sourcePath), ".cbz", StringComparison.CurrentCultureIgnoreCase))
+        return;
 
-      // TODO: If file is a zip and just needs the extension changes, then duplicate to new file with the proper extension. If Options.ReplaceOriginalFile is true then just rename the file instead of duplicating
-      // If file is not already a zip archive then extract and create a new zip archive with the contents of the original archive. If Options.ReplaceOriginalFile is set then delete original file
-
-      using (Stream stream = File.OpenRead(sourcePath))
-      using (var reader = ReaderFactory.Open(stream))
+      if (FileIsZip)
       {
-        while (reader.MoveToNextEntry())
-        {
-          if (!reader.Entry.IsDirectory)
-          {
-            Console.WriteLine(reader.Entry.Key);
+        if (File.Exists(convertedPath) && options.OverwriteExisting)
+          File.Delete(convertedPath);
 
-            reader.WriteEntryToDirectory(workingPath, new ExtractionOptions()
-            {
-              ExtractFullPath = true,
-              Overwrite = true
-            });
-          }
+        if (options.OverwriteExisting)
+        {
+          // TODO: rename existing file
+        }
+        else
+        {
+          // TODO: copy existing file to converted path
         }
       }
-
-      using (var archive = ZipArchive.Create())
+      else
       {
-        archive.AddAllFromDirectory(workingPath);
-        archive.DeflateCompressionLevel = SharpCompress.Compressors.Deflate.CompressionLevel.None;
-        archive.SaveTo(convertedPath, CompressionType.Deflate);
+        var workingPath = Path.Combine(options.WorkingPath ?? AppContext.BaseDirectory, sourceFilename);
+
+        using (Stream stream = File.OpenRead(sourcePath))
+        using (var reader = ReaderFactory.Open(stream))
+        {
+          while (reader.MoveToNextEntry())
+          {
+            if (!reader.Entry.IsDirectory)
+            {
+              Console.WriteLine(reader.Entry.Key);
+
+              reader.WriteEntryToDirectory(workingPath, new ExtractionOptions()
+              {
+                ExtractFullPath = true,
+                Overwrite = true
+              });
+            }
+          }
+        }
+
+        using (var archive = ZipArchive.Create())
+        {
+          archive.AddAllFromDirectory(workingPath);
+          archive.DeflateCompressionLevel = SharpCompress.Compressors.Deflate.CompressionLevel.None;
+          archive.SaveTo(convertedPath, CompressionType.Deflate);
+        }
+
+        Directory.Delete(workingPath, true);
       }
 
-      Directory.Delete(workingPath, true);
+      if (options.ReplaceOriginalFile && File.Exists(sourcePath))
+        File.Delete(sourcePath);
     }
   }
 }
