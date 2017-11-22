47442dc3ab356eb1f2103fa8c2ab41d58f8ad002
Craig Reynolds <craig@wyldphyre.com>
2017-11-22 20:15:02 +0800

    More implementation of ConvertToZipArchive()

--- ComicArchive/Converter.cs
+++ ComicArchive/Converter.cs
@@ -10,43 +10,75 @@ namespace ComicArchive
 {
   public class ConverterOptions
   {
+    public ConverterOptions()
+    {
+      this.LogOptions = new ConverterLogOptions();
+    }
     public bool ReplaceOriginalFile { get; set; }
     public string WorkingPath { get; set; }
     public bool OverwriteExisting { get; set; }
+    public readonly ConverterLogOptions LogOptions;
+  }
+
+  public class ConverterLogOptions
+  {
+    public bool ShowFullPaths { get; set;}
   }
 
   public class Converter
   {
-    public void ConvertToZipArchive(string sourcePath, ConverterOptions options, out string convertedPath)
+    void LogActivity(string message)
     {
-      if (!File.Exists(sourcePath))
-        throw new Exception($"Cannot convert '{sourcePath}. File does not exist.");
+      LogActivityEvent?.Invoke(message);
+    }
 
+    public (bool, string) ConvertToZipArchive(string sourcePath, ConverterOptions options, out string convertedPath)
+    {
+      convertedPath = string.Empty;
+
+      if (!File.Exists(sourcePath))
+        return (false, $"Cannot convert '{sourcePath}. File does not exist.");
+      
       var sourceDirectory = Path.GetDirectoryName(sourcePath);
       var sourceFilename = Path.GetFileNameWithoutExtension(sourcePath);
       convertedPath = Path.Combine(sourceDirectory, sourceFilename + ".cbz");
 
-      if (File.Exists(convertedPath) && !options.OverwriteExisting)
-        throw new Exception($"Cannot convert path '{sourcePath} to Zip. Target path '{convertedPath}' already exists");
+      var sourceDisplayPath = sourcePath;
+      var convertedDisplayPath = convertedPath;
+
+      if (!options.LogOptions.ShowFullPaths)
+      {
+        sourceDisplayPath = Path.GetFileName(sourceDisplayPath);
+        convertedDisplayPath = Path.GetFileName(convertedDisplayPath);
+      }
 
       var FileIsZip = ZipArchive.IsZipFile(sourcePath);
 
-      //TODO: Confirm that if an archive is already a zip file and has the proper extension then it is skipped
       if (FileIsZip && string.Equals(Path.GetExtension(sourcePath), ".cbz", StringComparison.CurrentCultureIgnoreCase))
-        return;
+      {
+        LogActivity($"Skipping '{sourceDisplayPath}'. Already a zip archive");
+        return (true, string.Empty);
+      }
+
+      // This is mostly a sanity check. Shouldn't come up.
+      if (string.Equals(sourcePath, convertedPath, StringComparison.CurrentCultureIgnoreCase))
+        return (false, $"Skipping because target and destination are the same: '{sourceDisplayPath}'");
+
+      if (File.Exists(convertedPath) && !options.OverwriteExisting)
+        return (false, $"Cannot convert path '{sourceDisplayPath} to Zip. Target path '{convertedDisplayPath}' already exists");
 
       if (FileIsZip)
       {
         if (File.Exists(convertedPath) && options.OverwriteExisting)
-          File.Delete(convertedPath);
-
-        if (options.OverwriteExisting)
         {
-          // TODO: rename existing file
+          File.Delete(convertedPath);
+          File.Copy(sourcePath, convertedPath);
+          LogActivity($"Replaced '{sourceDisplayPath}' with '{convertedDisplayPath}'");
         }
         else
         {
-          // TODO: copy existing file to converted path
+          File.Copy(sourcePath, convertedPath);
+          LogActivity($"Copied '{sourceDisplayPath}' to '{convertedDisplayPath}'");
         }
       }
       else
@@ -76,13 +108,21 @@ namespace ComicArchive
           archive.AddAllFromDirectory(workingPath);
           archive.DeflateCompressionLevel = SharpCompress.Compressors.Deflate.CompressionLevel.None;
           archive.SaveTo(convertedPath, CompressionType.Deflate);
+          LogActivity($"Created '{convertedDisplayPath}'");
         }
 
         Directory.Delete(workingPath, true);
       }
 
+      // TODO: Check replacement functionality works
       if (options.ReplaceOriginalFile && File.Exists(sourcePath))
+      {
         File.Delete(sourcePath);
+        LogActivity($"Deleted '{sourceDisplayPath}'");
+      }
+
+      return (true, string.Empty);
     }
+    public event Action<string> LogActivityEvent;
   }
 }
