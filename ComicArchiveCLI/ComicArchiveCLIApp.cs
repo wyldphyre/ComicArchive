f017b0b16253cf6a6130047db716c6caa2a559df
Craig Reynolds <wyldphyre@gmail.com>
2017-11-27 16:53:37 +0800

    Start handling command line parameters using .Net Command-Line Auto-Parser

---
+++ ComicArchiveCLI/ComicArchiveCLIApp.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CLAP;
+using CLAP.Validation;
+
+namespace ComicArchiveCLI
+{
+  class ComicArchiveCLIApp
+  {
+    [Verb(Description = "Read the metadata for a comic archive. Only supports reading Comic Rack metadata.")]
+    public static void Read(
+      [Required]
+      [FileExists] string filePath
+      )
+    {
+    }
+
+    [Verb(Description = "Convert non-zip comic archives into zip archives.")]
+    public static void Convert(
+      [Required]
+      [Description("The file or folder of files to convert")]
+      [PathExists] string sourcePath
+      )
+    {
+      var options = new ComicArchive.ConverterOptions
+      {
+        OverwriteExisting = true
+      };
+      //options.LogOptions.ShowFullPaths = true;
+
+      var converter = new ComicArchive.Converter();
+
+      converter.LogActivityEvent += (message) => Console.WriteLine(message);
+
+      try
+      {
+        var (result, message) = converter.ConvertToZipArchive(sourcePath, options, out var convertedPath);
+
+        // Some paths for testing
+        //@"F:\Media\Comics\Testing\Big Man Plans\Big Man Plans 01 (of 04) (2015) (digital) (Son of Ultron-Empire).cbr"
+        //@"/Users/craigreynolds/Desktop/comictest/Big Hero 6 01 (of 05) (2008) (Digital) (AnPymGold-Empire).cbz"
+        //(@"/Users/craigreynolds/Desktop/comictest/Big Hero 6 01 (of 05) (2008) (Digital) (AnPymGold-Empire).zip"
+
+        if (!string.IsNullOrWhiteSpace(message))
+          Console.WriteLine(message);
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine($"Exception: {e.Message}\n\n");
+        Console.WriteLine(e.StackTrace);
+      }
+
+      Console.WriteLine(string.Empty);
+      Console.WriteLine("Conversion finshed!");
+      // TODO: output some stats on files skipped and files converted
+    }
+
+    [Empty]
+    public static void ShowHelp()
+    {
+      Console.WriteLine("Help and usage goes here");
+    }
+
+    [Help]
+    public static void Help(string help)
+    {
+      Console.WriteLine(help);
+    }
+  }
+}
--- ComicArchiveCLI/Program.cs
+++ ComicArchiveCLI/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using CLAP;
 
 namespace ComicArchiveCLI
 {
@@ -6,32 +7,12 @@ namespace ComicArchiveCLI
   {
     static void Main(string[] args)
     {
-      var converter = new ComicArchive.Converter();
-      var options = new ComicArchive.ConverterOptions();
-      options.OverwriteExisting = true;
-      //options.LogOptions.ShowFullPaths = true;
+      Parser.Run<ComicArchiveCLIApp>(args);
 
-      // TODO: Implement command arguments of: Convert, Read, and (eventually) Write
-      
-      converter.LogActivityEvent += (message) => Console.WriteLine(message);
-
-      try
-      {
-        //converter.ConvertToZipArchive(@"F:\Media\Comics\Testing\Big Man Plans\Big Man Plans 01 (of 04) (2015) (digital) (Son of Ultron-Empire).cbr", options, out var convertedPath);
-        //var (result, message) = converter.ConvertToZipArchive(@"/Users/craigreynolds/Desktop/comictest/Big Hero 6 01 (of 05) (2008) (Digital) (AnPymGold-Empire).cbz", options, out var convertedPath);
-        //var (result, message) = converter.ConvertToZipArchive(@"/Users/craigreynolds/Desktop/comictest/Big Hero 6 01 (of 05) (2008) (Digital) (AnPymGold-Empire).zip", options, out var convertedPath);
-
-        // if (!string.IsNullOrWhiteSpace(message))
-        //   Console.WriteLine(message);
-      }
-      catch (Exception e)
-      {        
-        Console.WriteLine($"Exception: {e.Message}\n\n");
-        Console.WriteLine(e.StackTrace);
-      }
-
-      Console.WriteLine(string.Empty);
-      Console.WriteLine("ComicArchive finshed!");
+#if DEBUG
+      Console.WriteLine("Press ENTER to exit");
+      Console.ReadLine();
+#endif
     }
   }
 }
