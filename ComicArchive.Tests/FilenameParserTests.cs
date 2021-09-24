2d930fba843b6c348adc0ba4f608cd999f79451b
Craig Reynolds <craig@wyldphyre.com>
2021-09-24 21:28:31 +0800

    Implement tests for the rest of the data I want to be able to parse from the filename

--- ComicArchive.Tests/FilenameParserTests.cs
+++ ComicArchive.Tests/FilenameParserTests.cs
@@ -15,38 +15,75 @@ namespace ComicArchive.Tests
 
         [Theory]
         [MemberData(nameof(ParseSampleData))]
-        public void TestParse(string filename, ParsedFilenameData data)
+        public void TestParseOfVolume(string filename, ParsedFilenameData data)
         {
             FilenameParser.Parse(filename).Volume.ShouldBe(data.Volume);
+        }
+
+        [Theory]
+        [MemberData(nameof(ParseSampleData))]
+        public void TestParseOfNumber(string filename, ParsedFilenameData data)
+        {
+            FilenameParser.Parse(filename).Number.ShouldBe(data.Number);
+        }
+
+        [Theory]
+        [MemberData(nameof(ParseSampleData))]
+        public void TestParseOfSeries(string filename, ParsedFilenameData data)
+        {
+            FilenameParser.Parse(filename).Series.ShouldBe(data.Series);
+        }
+
+        [Theory]
+        [MemberData(nameof(ParseSampleData))]
+        public void TestParseOfYear(string filename, ParsedFilenameData data)
+        {
+            FilenameParser.Parse(filename).Year.ShouldBe(data.Year);
+        }
+
+        [Theory]
+        [MemberData(nameof(ParseSampleData))]
+        public void TestParseOfArtist(string filename, ParsedFilenameData data)
+        {
+            FilenameParser.Parse(filename).Artist.ShouldBe(data.Artist);
+        }
+
+        [Theory]
+        [MemberData(nameof(ParseSampleData))]
+        public void TestParseOfName(string filename, ParsedFilenameData data)
+        {
+            FilenameParser.Parse(filename).Name.ShouldBe(data.Name);
         }
 
         public static TheoryData<string, ParsedFilenameData> ParseSampleData = new TheoryData<string, ParsedFilenameData>()
         {
             // Common filename patterns
-            { "Vampeerz v1 ch01.cbz", new ParsedFilenameData { Volume = 1 } },
-            { "Assassination Classroom v01 (2014) (Digital) (Lovag-Empire).cbz", new ParsedFilenameData { Volume = 1 } },
-            { "Assassination Classroom v01.cbz", new ParsedFilenameData { Volume = 1 } },
-            { "Assassination Classroom v10.cbz", new ParsedFilenameData { Volume = 10 } },
-            { "Assassination Classroom vol01.cbz", new ParsedFilenameData { Volume = 1 } },
-            { "Assassination Classroom vol10.cbz", new ParsedFilenameData { Volume = 10 } },
-            { "Assassination Classroom vol 10.cbz", new ParsedFilenameData { Volume = 10 } },
-            { "Assassination Classroom vol NotA Volume.cbz", new ParsedFilenameData { } },
-            { "Assassination Classroom 01.cbz", new ParsedFilenameData { } },
-            { "Assassination Classroom ch01.cbz", new ParsedFilenameData { } },
-            { "Assassination Classroom ch 01.cbz", new ParsedFilenameData { } },
-            { "Ah My Goddess 1.cbz", new ParsedFilenameData { } },
-            { "Ah My Goddess 10.cbz", new ParsedFilenameData { } },
-            { "Claymore 001 - Silver-eyed Slayer[m-s].cbz", new ParsedFilenameData { } }, // : includes chapter name
-            { "Claymore 002 - Claws in the Sky[m-s].cbz", new ParsedFilenameData { } }, // : includes chapter name
-            { "[Tokuwotsumu] Tea Brown and Milk Tea [TZdY].cbz", new ParsedFilenameData { } },
-            { "(Isoya Yuki) The Day the Cherryfruit Ripens (Hirari 14) [project].cbz", new ParsedFilenameData { } },
-            { "[Garun] I Could Just Tell.cbz", new ParsedFilenameData { } },
-            { "[Takemiya Jin] Yaezakura Sympathy 1 [TZdY].cbz", new ParsedFilenameData { } },
-            { "04.cbz", new ParsedFilenameData { } },
-            { "05 - Let's Be Careful With Summer.cbz", new ParsedFilenameData { } }, // : assume chapter number and name
-            { "2000 AD 0001.cbz", new ParsedFilenameData { } },
-            { "2000 AD 0345 (Cclay).cbz", new ParsedFilenameData { } },
-            { "Ascender 001 (2019) (Digital) (Zone-Empire).cbz", new ParsedFilenameData { } },
+            { "Vampeerz v1 ch01.cbz", new ParsedFilenameData { Volume = 1, Number = "1", Series = "Vampeerz" } },
+            { "Assassination Classroom v01 (2014) (Digital) (Lovag-Empire).cbz", new ParsedFilenameData { Volume = 1, Year = 2014, Series = "Assassination Classroom" } },
+            { "Assassination Classroom v01.cbz", new ParsedFilenameData { Volume = 1, Series = "Assassination Classroom" } },
+            { "Assassination Classroom v10.cbz", new ParsedFilenameData { Volume = 10, Series = "Assassination Classroom" } },
+            { "Assassination Classroom vol01.cbz", new ParsedFilenameData { Volume = 1, Series = "Assassination Classroom" } },
+            { "Assassination Classroom vol10.cbz", new ParsedFilenameData { Volume = 10, Series = "Assassination Classroom" } },
+            { "Assassination Classroom vol 10.cbz", new ParsedFilenameData { Volume = 10, Series = "Assassination Classroom" } },
+            { "Assassination Classroom vol NotA Volume.cbz", new ParsedFilenameData { Series = "Assassination Classroom" } },
+            { "Assassination Classroom 01.cbz", new ParsedFilenameData { Number = "1", Series = "Assassination Classroom" } },
+            { "Assassination Classroom ch01.cbz", new ParsedFilenameData { Number = "1", Series = "Assassination Classroom" } },
+            { "Assassination Classroom ch 01.cbz", new ParsedFilenameData { Number = "1", Series = "Assassination Classroom" } },
+            { "Ah My Goddess 1.cbz", new ParsedFilenameData { Number = "1", Series = "Assassination Ah My Goddess" } },
+            { "Ah My Goddess 10.cbz", new ParsedFilenameData { Number = "10", Series = "Assassination Ah My Goddess" } },
+            { "Claymore 001 - Silver-eyed Slayer[m-s].cbz", new ParsedFilenameData { Number = "1", Name = "Silver-eyed Slayer", Series = "Claymore" } }, // : includes chapter name
+            { "Claymore 002 - Claws in the Sky[m-s].cbz", new ParsedFilenameData { Number = "2", Name = "Claws in the sky", Series = "Claymore" } }, // : includes chapter name
+            { "[Tokuwotsumu] Tea Brown and Milk Tea [TZdY].cbz", new ParsedFilenameData { Series = "Tea Brown and Milk Tea" } },
+            { "(Isoya Yuki) The Day the Cherryfruit Ripens (Hirari 14) [project].cbz", new ParsedFilenameData { Series = "The Day the Cherryfruit Ripens", Artist = "Isoya Yuki" } },
+            { "[Garun] I Could Just Tell.cbz", new ParsedFilenameData { Series = "I Could Just Tell", Artist = "Garun" } },
+            { "[Takemiya Jin] Yaezakura Sympathy 1 [TZdY].cbz", new ParsedFilenameData { Artist = "Takemiya Jin", Series = "Yaesakura Sympathy", Number = "1" } },
+            { "04.cbz", new ParsedFilenameData { Number = "4" } },
+            { "vol04.cbz", new ParsedFilenameData { Volume = 4 } },
+            { "vol 04.cbz", new ParsedFilenameData { Volume = 4 } },
+            { "05 - Let's Be Careful With Summer.cbz", new ParsedFilenameData { Number = "5", Name = "Let's Be Careful With Summer" } }, // : assume chapter number and name
+            { "2000 AD 0001.cbz", new ParsedFilenameData { Number = "1", Series = "2000 AD" } },
+            { "2000 AD 0345 (Cclay).cbz", new ParsedFilenameData { Number = "345", Series = "2000 AD" } },
+            { "Ascender 001 (2019) (Digital) (Zone-Empire).cbz", new ParsedFilenameData { Number = "1", Series = "Ascender", Year = 2019 } },
         };
 
         public static TheoryData<string, string[]> TokenisedFilenameData = new TheoryData<string, string[]>()
--- ComicArchive/ParsedFilenameData.cs
+++ ComicArchive/ParsedFilenameData.cs
@@ -11,6 +11,8 @@
         public string Name { get; set; }
 
         public string Artist { get; set; }
+
+        public int Year { get; set; }
     }
 
 }
